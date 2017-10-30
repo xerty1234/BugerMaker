54018cbee8deb71826fb711fb595e56804d89bf7
xerty1234 <xerty1234@naver.com>
2017-10-30 14:48:02 +0900

    프로젝트 생성
    프로젝트 생성 및 빌드

---
+++ Assets/Source/Create_Bugker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Create_Bugker : MonoBehaviour
+{
+
+    // 버거의 아이콘들을 배치하는 스크립트
+   // public int MAX_BM_NodeCount;
+    public GameObject [] BM_Node;
+    Vector2 Pos;
+	
+	void Start ()
+    {
+       // BM_Node = new GameObject[MAX_BM_NodeCount];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log(Input.mousePosition.x);
+            Debug.Log(Input.mousePosition.y);
+        }
+
+	}
+}
